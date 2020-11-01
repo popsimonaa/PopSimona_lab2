6d7370bde179662fa5158823b3c99cc14d89b9d4
simona pop <pop.simona.ps@gmail.com>
2020-11-01 13:55:11 +0200

    schimbare

---
+++ Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PopSimona_lab2
+{
+    class Class1
+    {
+    }
+}
---
+++ StopCommand.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace PopSimona_lab4.CustomCommands
+{
+    class StopCommand
+    {
+        private static RoutedUICommand launch_command;
+        static StopCommand()
+        {
+            InputGestureCollection myInputGestures = new InputGestureCollection();
+            launch_command = new RoutedUICommand("Launch", "Launch", typeof(StopCommand), myInputGestures);
+            myInputGestures.Add(new KeyGesture(Key.S, ModifierKeys.Control));
+        }
+        public static RoutedUICommand Launch
+        {
+            get
+            {
+                return launch_command;
+            }
+        }
+    }
+}
